43d7118c108744e087e0cc144b1c9625972b13b1
eunxoo <dmstn7989@naver.com>
2023-05-13 15:51:20 +0900

    [2-4] 엔티티 클래스 개발2
    Category 엔티티 추가
    Item.java & Address.java 수정

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {   // jPA 스펙 상 만들어둔 것. 보통 호출 안함-> protected로 만듦
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.persistence.GeneratedValue;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -23,5 +26,8 @@ public abstract class Item {
     private int stock;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
