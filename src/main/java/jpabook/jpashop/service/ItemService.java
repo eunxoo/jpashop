e77681ccbc6ea4d41328ebc01b8171f360186553
eunxoo <dmstn7989@naver.com>
2023-05-28 22:49:42 +0900

    [7-7] 변경 감지와 병합(merge)
    ** 중요 **

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional // commit할 때 업데이트 됨. -> 변경 감지
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) { // param : 파라미터로 넘어온 준영속 상태의 엔티티
+        Item findItem = itemRepository.findOne(itemId); // 영속성 있는 Item
+        //findItem.chang(price, name, stockQuantity) 실무에서는 set 대신 이렇게 의미있는 함수를 사용해야함.
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
