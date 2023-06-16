7eaa7db1004eb37a1f42eff2e07c38c4c34acbc3
eunxoo <dmstn7989@naver.com>
2023-06-17 00:14:26 +0900

    [1-3] 회원 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Entity
 @Getter @Setter
 public class Member {
@@ -18,7 +17,6 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @NotEmpty
     private String name;
 
     @Embedded
